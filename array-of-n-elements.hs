d186dc7868f21f79f61bbbb957ed8a4e6b52a4a3
Radomir Stevanovic <radomir.stevanovic@gmail.com>
2017-06-04 16:24:53 +0200

    add: array of n elements

---
+++ array-of-n-elements.hs
@@ -0,0 +1,11 @@
+{-
+Array Of N Elements (https://www.hackerrank.com/challenges/fp-array-of-n-elements)
+FP/Introduction, Easy, 10 pts
+
+Create an array of n integers, where the value of n is passed as an argument to
+the pre-filled function in your editor. This challenge uses a custom checker,
+so you can create any array of n integers. For example, if n=4, you could return
+[1,1,1,1], [1,2,3,4], or any other array of equal length.
+-}
+
+fn n = [1..n]
