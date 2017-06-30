0cbb9a878d93bf1643d7c93c12ec527d45a56743
Radomir Stevanovic <radomir.stevanovic@gmail.com>
2017-06-30 23:16:58 +0200

    update: fibonacci with linear (list-based) memoization

--- fibonacci.hs
+++ fibonacci.hs
@@ -11,12 +11,31 @@ Constraints:
 
 import Data.Function.Memoize
 
-fib :: Integer -> Integer
+
+-- memoization with `memoize`, using a tree structure for faster lookups
+fib :: Int -> Integer
 fib = memoize fib'
   where
     fib' 0 = 0
     fib' 1 = 1
     fib' n = (fib (n-1) + fib (n-2)) `mod` 100000007
+
+
+-- manual memoization using a linear structure for lookups (list)
+fibl :: Int -> Integer
+fibl = (map fib' [0..] !!)
+  where
+    fib' 0 = 0
+    fib' 1 = 1
+    fib' n = (fibl (n-1) + fibl (n-2)) `mod` 100000007
+
+
+-- plain non-memoized fibonacci (slow)
+fibs :: Int -> Integer
+fibs 0 = 0
+fibs 1 = 1
+fibs n = (fibs (n-1) + fibs (n-2)) `mod` 100000007
+
 
 main :: IO ()
 main = do
